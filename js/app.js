818e459cca3c2d6191e6fad743e85b6cb2305f35
Rich Greenhill <rgnet1@yahoo.com>
2016-03-22 14:50:13 -0700

    ryu ready/still on mouse move

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-  console.log('javascript loaded!');  
+  $('.ryu').mouseenter(function(){
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function(){
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+  })
 });
 
